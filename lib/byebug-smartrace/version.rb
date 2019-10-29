6bcdc3b409fbeef09edd41294f402a9e7e2cec19
Nikolay Rys <nikolay@rys.me>
2019-10-29 04:44:44 +0100

    More consistent approach to frozen strings

---
+++ lib/byebug-smartrace/version.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+module Byebug
+  module Smarttrace
+    VERSION = '0.0.0'
+  end
+end
--- lib/byebug-smarttrace/version.rb
+++
@@ -1,5 +0,0 @@
-module Byebug
-  module Smarttrace
-    VERSION = '0.0.0'.freeze
-  end
-end
