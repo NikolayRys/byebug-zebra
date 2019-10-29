54563f5ffaf2621a30d60af87130769e782da7c1
Nikolay Rys <nikolay@rys.me>
2019-10-29 01:06:11 +0100

    Move the main lib file

---
+++ lib/byebug-smarttrace.rb
@@ -0,0 +1,13 @@
+require 'byebug-smarttrace/version'
+require 'byebug-smarttrace/smarttrace_command'
+require 'byebug-smarttrace/pry_smarttrace_command' if defined? PryByebug
+
+
+# TODO: exception extending policy?
+# class Error < StandardError; end
+
+module Byebug
+  module Smarttrace
+
+  end
+end
