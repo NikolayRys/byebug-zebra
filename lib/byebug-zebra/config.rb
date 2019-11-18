e37b09864527cfd6105c71264361a4aab169ab6f
Nikolay Rys <nikolay@rys.me>
2019-11-18 05:36:50 +0100

    Reword warning in config

--- lib/byebug-zebra/config.rb
+++ lib/byebug-zebra/config.rb
@@ -16,7 +16,7 @@ module ByebugZebra
     end
 
     ROOT_WARNING = <<-DESCRIPTION
-WARNING: application root directory is not set, so using Dir.pwd. Please specify the correct path via the config:
+WARNING: application root directory is not set, using Dir.pwd. Specify the correct root:
          ByebugZebra.config do |config|
            config.root = '/abs/path/to/your/app'
          end
