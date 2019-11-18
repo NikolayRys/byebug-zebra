ea7bef0485f357c9f5b7b9dbd0c552a902b392e8
Nikolay Rys <nikolay@rys.me>
2019-11-18 05:30:20 +0100

    Improve configuration

--- lib/byebug-zebra/config.rb
+++ lib/byebug-zebra/config.rb
@@ -4,29 +4,28 @@ module ByebugZebra
   class Config
 
     # Colors that can be used: ColorizedString.colors
-    attr_accessor :color, :root, :known_paths, :odd_color, :even_color, :warn_color
+    attr_accessor :color, :root, :known_libs, :odd_color, :even_color, :warn_color
 
     def initialize
       @color = true
       @root = defined?(Rails) ? Rails.root.to_s : nil
-      @known_paths = {}
+      @known_libs = {}
       @odd_color = :light_white
       @even_color = :black
       @warn_color = :red
     end
 
     ROOT_WARNING = <<-DESCRIPTION
-    WARNING: application root directory is not set, thus using Dir.pwd. p
-             Please specify the correct path via the config:
-             ByebugZebra.config do |config|
-               config.root = '/abs/path/to/your/app'
-             end
+WARNING: application root directory is not set, so using Dir.pwd. Please specify the correct path via the config:
+         ByebugZebra.config do |config|
+           config.root = '/abs/path/to/your/app'
+         end
     DESCRIPTION
 
-    def enforce_root
+    def enforce_root!
       unless @root
-        msg = color ? ColorizedString[ROOT_WARNING].colorize(warn_color) : ROOT_WARNING
-        print msg # TODO: puts?
+
+        print(color ? ColorizedString[ROOT_WARNING].colorize(warn_color) : ROOT_WARNING)
         @root = Dir.pwd
       end
     end
