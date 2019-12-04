899153d61019a4550399a5b7e5d37f8e86921742
Nikolay Rys <nikolay@rys.me>
2019-12-04 14:31:28 +0100

    Improve config

--- lib/byebug-zebra/config.rb
+++ lib/byebug-zebra/config.rb
@@ -4,22 +4,25 @@ module ByebugZebra
   class Config
 
     # Colors that can be used: ColorizedString.colors
-    attr_accessor :color, :root, :known_libs, :odd_color, :even_color, :warn_color
+    attr_accessor :color, :root, :odd_color, :even_color, :warn_color, :normalize
+    attr_reader :known_libs, :stdlib_names
 
     def initialize
       @color = true
       @root = defined?(Rails) ? Rails.root.to_s : nil
-      @known_libs = {}
       @odd_color = :light_white
-      @even_color = :black
+      @even_color = :light_black
       @warn_color = :red
+      @normalize = false
+      @known_libs = {}
+      @stdlib_names = (Dir.entries(RbConfig::CONFIG['rubylibdir']) - ['.', '..', RbConfig::CONFIG['arch']]).map{|file| File.basename(file, File.extname(file))}.uniq
     end
 
     ROOT_WARNING = <<-DESCRIPTION
-WARNING: application root directory is not set, using Dir.pwd. 
-EXAMPLE: ByebugZebra.config do |config|
-           config.root = '/abs/path/to/your/app'
-         end
+WARNING: application root directory is not set, using Dir.pwd. Please do it like so:
+  ByebugZebra.config do |config|
+    config.root = '/abs/path/to/your/app'
+  end\n
     DESCRIPTION
 
     def ensure_root!
@@ -27,6 +30,10 @@ EXAMPLE: ByebugZebra.config do |config|
         print(color ? ColorizedString[ROOT_WARNING].colorize(warn_color) : ROOT_WARNING)
         @root = Dir.pwd
       end
+    end
+
+    def known_libs=(libs_hash)
+      @known_libs = libs_hash.to_h.transform_values{|path| Pathname.new(File.realpath(path))}
     end
 
   end
