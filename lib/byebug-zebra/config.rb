231c2be06edec6d66986b7809cb4abe529a4d1fb
Nikolay Rys <nikolay@rys.me>
2019-12-05 09:20:11 +0100

    Remove useless options from config

--- lib/byebug-zebra/config.rb
+++ lib/byebug-zebra/config.rb
@@ -4,22 +4,20 @@ module ByebugZebra
   class Config
 
     # Colors that can be used: ColorizedString.colors
-    attr_accessor :color, :root, :odd_color, :even_color, :warn_color, :normalize
+    attr_accessor :root, :odd_color, :even_color, :warn_color
     attr_reader :known_libs, :stdlib_names
 
     def initialize
-      @color = true
       @root = defined?(Rails) ? Rails.root.to_s : nil
       @odd_color = :light_white
       @even_color = :light_black
       @warn_color = :red
-      @normalize = false
       @known_libs = {}
       @stdlib_names = (Dir.entries(RbConfig::CONFIG['rubylibdir']) - ['.', '..', RbConfig::CONFIG['arch']]).map{|file| File.basename(file, File.extname(file))}.uniq
     end
 
     ROOT_WARNING = <<-DESCRIPTION
-WARNING: application root directory is not set, using Dir.pwd. Please do it like so:
+WARNING: application root directory is not set, using Dir.pwd. Example:
   ByebugZebra.config do |config|
     config.root = '/abs/path/to/your/app'
   end\n
@@ -27,7 +25,7 @@ WARNING: application root directory is not set, using Dir.pwd. Please do it like
 
     def ensure_root!
       unless @root
-        print(color ? ColorizedString[ROOT_WARNING].colorize(warn_color) : ROOT_WARNING)
+        print(ColorizedString[ROOT_WARNING].colorize(warn_color))
         @root = Dir.pwd
       end
     end
