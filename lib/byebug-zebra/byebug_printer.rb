32ecba508afc9b54ede9fd1ab96d541a97359992
Nikolay Rys <nikolay@rys.me>
2019-12-08 10:45:31 +0100

    Make complete overhaul of the backtrace printing logic

--- lib/byebug-zebra/byebug_printer.rb
+++ lib/byebug-zebra/byebug_printer.rb
@@ -3,81 +3,112 @@ require 'colorized_string'
 module ByebugZebra
   class ByebugPrinter
 
+    RUBY_DIR   = RbConfig::CONFIG['prefix']
+    STDLIB_DIR = Pathname.new(RbConfig::CONFIG['rubylibdir'])
+
+    ORIGIN_WARNING  = <<-DESCRIPTION
+WARNING: Origin of some stack frames have not been recognized. Specify them in the config. Example:
+  ByebugZebra.config do |config|
+    config.known_libs = {lib1: '/abs/path/to/your/lib1', lib2: '/abs/path/to/your/lib2' }
+  end
+DESCRIPTION
+
     def initialize(stack_size, &frame_block)
       @stack_size     = stack_size
       @frame_block    = frame_block
-      @odd_scheme     = { color: config.odd_color, background: config.even_color }
-      @even_scheme    = { color: config.even_color, background: config.odd_color }
-      @unknown_origin = false
+
+      # TODO: cache on load
+      @external_gems  = Gem.loaded_specs.values.reject(&:default_gem?).map{|spec| [spec.name, Pathname.new(spec.full_gem_path)]}
+
+      @schemes = {
+        odd: {
+          basic: { color: config.even_color, background: config.odd_color },
+          unknown: { color: config.warn_color, background: config.odd_color },
+          application: { color: config.even_color, background: config.odd_color, mode: :italic }
+        },
+        even: {
+          basic:  { color: config.odd_color, background: config.even_color },
+          unknown: { color: config.warn_color, background: config.even_color },
+          application: { color: config.odd_color, background: config.even_color, mode: :italic }
+        }
+      }
     end
 
     def print_striped_backtrace
-      current_origin = nil
-      current_oddness = false
+      prev_origin = nil
+      odd = false
+      unknown_detected = false
 
+      parsed_frames = []
       @stack_size.times do |i|
-        frame_hash = get_frame_hash_with_origin(@frame_block.call(i))
-        unless current_origin == frame_hash[:origin]  ## OR UNKNOWN
-          current_oddness = !current_oddness
-          current_origin = frame_hash[:origin]
-        end
-        puts frame_str(frame_hash, current_oddness)
+        frame = @frame_block.call(i)
+        origin = analyze_origin(frame)
+        unknown_detected = true if origin.first == :unknown
+        odd = !odd unless origin == prev_origin
+        prev_origin = origin
+        parsed_frames << str_parts(frame, origin).concat([origin, odd])
       end
-      puts to_warn(ORIGIN_WARNING) if @unknown_origin
+      puts ColorizedString[ORIGIN_WARNING].colorize(config.warn_color) if unknown_detected
+      parsed_frames.each { |frame_args| print_frame_line(*frame_args) }
     end
-
-    ORIGIN_WARNING = 'WARNING: Origin of some stack frames have not been recognized. Please provide paths for them.'
-    INFO_TEMPLATE  = '%{call} at %{file}:%{line}'
-    NUM_TEMPLATE   = '#%{pos}'
-    FRAME_TEMPLATE = '%{mark} %{num_str} %{info_str} in %{origin}'
 
     private
-    def frame_str(frame_hash, odd)
-      frame_hash[:num_str] = NUM_TEMPLATE % frame_hash
-      colorize(FRAME_TEMPLATE % frame_hash, odd ? @odd_scheme : @even_scheme)
-    end
 
-    def get_frame_hash_with_origin(frame)
+    def str_parts(frame, origin)
       frame_hash = frame.to_hash
-      info_content = INFO_TEMPLATE % frame_hash
-
-      frame_path = File.expand_path(frame.file)
-      if origin_pair = config.known_libs.detect{|_name, libpath| is_subpath?(frame_path, libpath)}
-        frame_hash[:origin]   = "LIB (#{origin_pair.first})"
-        frame_hash[:info_str] = info_content
-      elsif is_subpath?(frame_path, config.root)
-        frame_hash[:origin]   = 'APPLICATION'
-        frame_hash[:info_str] = ColorizedString[info_content].bold
-      elsif gem_spec_pair = Gem.loaded_specs.detect{|_name, gemspec| is_subpath?(frame_path, gemspec.full_gem_path) }
-        frame_hash[:origin]   = "GEM (#{gem_spec_pair.first})"
-        frame_hash[:info_str] = info_content
-      elsif is_subpath?(frame_path, RbConfig::CONFIG['rubylibdir'])
-        frame_hash[:origin]   = 'STDLIB'
-        frame_hash[:info_str] = info_content
+      origin_str = case origin.first
+      when :gem, :lib, :stdlib
+        "#{origin.first.to_s.upcase}: #{origin.last}"
       else
-        @unknown_origin       = true
-        frame_hash[:origin]   = 'UNKNOWN'
-        frame_hash[:info_str] = to_warn(info_content)
+        origin.first.to_s.upcase
       end
-      frame_hash
+      ["#{frame_hash[:mark]} ##{frame_hash[:pos]} ",
+       "#{frame_hash[:call]} at #{frame_hash[:file]}:#{frame_hash[:line]} from #{origin_str}"]
     end
 
-    def is_subpath?( target, root )
-      real_root = File.realpath(root) rescue nil
-      real_root && target.start_with?(real_root)
+    def print_frame_line(prefix_str, info_str, origin, odd)
+      scheme_group = odd ? :odd : :even
+      info_scheme = [:application, :unknown].include?(origin.first) ? origin.first : :basic
+
+      puts ColorizedString[prefix_str].colorize(@schemes[scheme_group][:basic]) +
+        ColorizedString[info_str].colorize(@schemes[scheme_group][info_scheme])
     end
 
-    def colorize(text, color)
-      config.color ? ColorizedString[text].colorize(color) : text
+    # TODO: move out to analyzer
+    def subpath?(target_path, root_path)
+      # Is this exactly this file or a file in subdirectory
+      target_path.fnmatch?("#{root_path}{#{File::SEPARATOR}**,}", File::FNM_EXTGLOB)
     end
 
-    def to_warn(text)
-      colorize(ColorizedString[text].italic, config.warn_color)
+    def stdlib?(frame_path)
+      if subpath?(frame_path, STDLIB_DIR)
+        internal_subpath = frame_path.relative_path_from(STDLIB_DIR).to_s
+        config.stdlib_names.detect{|name| internal_subpath.start_with?(name)}
+      end
     end
+
+    def analyze_origin(frame)
+      frame_path = Pathname.new(frame.file)
+      if frame.c_frame?
+        [:native, frame.file]
+      elsif origin_pair = config.known_libs.detect{|_name, lib_path| subpath?(frame_path, lib_path)}
+        [:lib, origin_pair.first]
+      elsif subpath?(frame_path, config.root)
+        [:application]
+      elsif gem_pair = @external_gems.detect{|_name, gem_path| subpath?(frame_path, gem_path) }
+        [:gem, gem_pair.first]
+      elsif std_name = stdlib?(frame_path)
+        [:stdlib, std_name]
+      elsif subpath?(frame_path, RUBY_DIR)
+        [:core, frame.file]
+      else
+        [:unknown, frame.file]
+      end
+    end
+    # ---------
 
     def config
       ByebugZebra.config
     end
-
   end
 end
