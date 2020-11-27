6a84bf055731278394977caf2d54a3e4e8fdb2a9
Nikolay Rys <nikolay@rys.me>
2020-11-27 02:09:47 +0100

    Move stuff out to analyzer

--- lib/byebug-zebra/byebug_printer.rb
+++ lib/byebug-zebra/byebug_printer.rb
@@ -1,5 +1,8 @@
 require 'colorized_string'
 require 'tty-prompt'
+require 'byebug-zebra/analyzer'
+
+# TODO: use Pathname for paths
 
 module ByebugZebra
   class ByebugPrinter
@@ -7,19 +10,18 @@ module ByebugZebra
     RUBY_DIR   = RbConfig::CONFIG['prefix']
     STDLIB_DIR = Pathname.new(RbConfig::CONFIG['rubylibdir'])
 
-    ORIGIN_WARNING  = <<-DESCRIPTION
+    ORIGIN_WARNING  = <<-TEXT
 WARNING: Origin of some stack frames have not been recognized. Specify them in the config. Example:
   ByebugZebra.config do |config|
     config.known_libs = {lib1: '/abs/path/to/your/lib1', lib2: '/abs/path/to/your/lib2' }
   end
-DESCRIPTION
+TEXT
 
-    def initialize(stack_size, &frame_block)
-      @stack_size     = stack_size
-      @frame_block    = frame_block
+    def initialize(context)
+      @context = context
 
-      # TODO: cache on load
-      @external_gems  = Gem.loaded_specs.values.reject(&:default_gem?).map{|spec| [spec.name, Pathname.new(spec.full_gem_path)]}
+      # TODO: cache on load in config
+      @loaded_external_gems  = Gem.loaded_specs.values.reject(&:default_gem?).map{|spec| [spec.name, Pathname.new(spec.full_gem_path)]}
 
       @schemes = {
         odd: {
@@ -43,9 +45,9 @@ DESCRIPTION
       @origins = []
 
       parsed_frames = []
-      @stack_size.times do |i|
-        frame = @frame_block.call(i)
-        origin = analyze_origin(frame)
+      @context.stack_size.times do |index|
+        frame = Byebug::Frame.new(@context, index)
+        origin = analyze_origin(frame) # Move to the module
         @origins << origin
 
         unknown_detected = true if origin.first == :unknown
@@ -56,7 +58,13 @@ DESCRIPTION
       puts ColorizedString[ORIGIN_WARNING].colorize(config.warn_color) if unknown_detected
       parsed_frames.each { |frame_args| print_frame_line(*frame_args) }
 
+      filter_prompt
 
+    end
+
+    private
+
+    def filter_prompt
       TTY::Prompt.new.multi_select('Which sources should zebra ignore?', cycle: true) do |menu|
         menu.default 1
 
@@ -65,8 +73,6 @@ DESCRIPTION
         end
       end
     end
-
-    private
 
     def str_parts(frame, origin)
       frame_hash = frame.to_hash
@@ -89,36 +95,7 @@ DESCRIPTION
     end
 
     # TODO: move out to analyzer
-    def belongs?(target_path, root_path)
-      # Is this exactly this file or a file in subdirectory
-      target_path.fnmatch?("#{root_path}{#{File::SEPARATOR}**,}", File::FNM_EXTGLOB)
-    end
 
-    def stdlib?(frame_path)
-      if belongs?(frame_path, STDLIB_DIR)
-        internal_subpath = frame_path.relative_path_from(STDLIB_DIR).to_s
-        config.stdlib_names.detect{|name| internal_subpath.start_with?(name)}
-      end
-    end
-
-    def analyze_origin(frame)
-      frame_path = Pathname.new(frame.file)
-      if frame.c_frame?
-        [:native, frame.file]
-      elsif origin_pair = config.known_libs.detect{|_name, lib_path| belongs?(frame_path, lib_path)}
-        [:lib, origin_pair.first]
-      elsif belongs?(frame_path, config.root)
-        [:application]
-      elsif gem_pair = @external_gems.detect{|_name, gem_path| belongs?(frame_path, gem_path) }
-        [:gem, gem_pair.first]
-      elsif std_name = stdlib?(frame_path)
-        [:stdlib, std_name]
-      elsif belongs?(frame_path, RUBY_DIR)
-        [:core, frame.file]
-      else
-        [:unknown, frame.file]
-      end
-    end
     # ---------
 
     def config
